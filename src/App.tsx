cca6ef7335998f94cb65cf84fd912073b553d28b
Your Name <user@example.com>
2020-04-03 00:59:27 +0000

    Add framerate counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import * as R from 'ramda';
 import React from 'react';
 import './App.css';
 import Infektor, { Person } from './infektor';
 
-const frameDelay = 100;
+const targetFps = 10
+const frameDelay = 1000 / targetFps;
 
-const initialPopulation: Person[] =  []
+const initialPopulation: Person[] = []
 
 const zoom = 2
 
@@ -16,33 +18,52 @@ for (let i = 0; i < 1000; i++) {
   })
 }
 
+const pushDateAndLimit = (frames: number[]) => {
+  const newFrames = R.prepend(Date.now(), frames);
+  return R.slice(0, 5, newFrames)
+}
+
+const framerate = (frames: number[]) => {
+  if (R.isEmpty(frames)) return 0;
+
+  let totalDistance = 0
+  for (let i = 0; i < frames.length - 2; i++) {
+    totalDistance += frames[i] - frames[i + 1]
+  }
+  return Math.round(1000 / (totalDistance / frames.length))
+}
+
 function App() {
+  const [frames, setFrames] = React.useState<number[]>([]);
   const [population, setPopulation] = React.useState(initialPopulation)
 
   React.useEffect(() => {
     const infektor = new Infektor({ population: initialPopulation, zoom })
+    let stop = false
 
     const animate = () => {
       infektor.step();
       setPopulation(infektor.getPopulation())
-      timeout = setTimeout(animate, frameDelay)
+      if (stop) return;
+      setFrames(f => pushDateAndLimit(f))
+      setTimeout(animate, frameDelay)
     }
+    animate()
 
-    let timeout = setTimeout(animate, frameDelay);
-
-    return () => clearTimeout(timeout) 
+    return () => { stop = true }
   }, [])
 
   return (
     <div className="App">
+      <p className="framerate">{framerate(frames)}</p>
       <svg viewBox={`0 0 ${100 / zoom} ${100 / zoom}`}>
-        {population.map(({x, y, infected}, i) => (
-          <circle 
-            key={i} 
-            cx={x} 
-            cy={y} 
-            fill={infected ? 'red' : 'green' } 
-            r="0.5"/>
+        {population.map(({ x, y, infected }, i) => (
+          <circle
+            key={i}
+            cx={x}
+            cy={y}
+            fill={infected ? 'red' : 'green'}
+            r="0.5" />
         ))}
       </svg>
     </div>
