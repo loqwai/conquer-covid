073fb97771f782f574c72160ffba82d70ad5349e
royvandewater <royvandewater@users.noreply.github.com>
2020-04-03 20:56:00 +0000

    deploy: 94032e8cb9fcd12f30fc54269bc816ca6dbb63d8

---
+++ precache-manifest.53dac28657aad6a796b71ffeda941863.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb36a017831968879b81549e33ebded",
+    "url": "/odds-stabilizer/index.html"
+  },
+  {
+    "revision": "0781a5d4b37fd615b379",
+    "url": "/odds-stabilizer/static/css/main.3267944d.chunk.css"
+  },
+  {
+    "revision": "7a7ce5a4c98fe7d2e0c8",
+    "url": "/odds-stabilizer/static/js/2.dfaab5bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/odds-stabilizer/static/js/2.dfaab5bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0781a5d4b37fd615b379",
+    "url": "/odds-stabilizer/static/js/main.0c5dff9a.chunk.js"
+  },
+  {
+    "revision": "bf76711ac57f63feeef7",
+    "url": "/odds-stabilizer/static/js/runtime-main.8713c291.js"
+  }
+]);
