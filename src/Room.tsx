bb27abab56e33332865bff9bcdca6398f94c91b2
redaphid <iam@aaronherres.com>
2020-04-09 16:22:25 -0700

    fix dumb filenames

--- src/Room.tsx
+++ src/Room.tsx
@@ -4,15 +4,11 @@ import SVGRenderer from './SVGRenderer'
 import useInterval from './hooks/useInterval'
 const RoomUI = () => {    
     const [room, setRoom] = React.useState<Room>(new Room(10, { height: 100, width: 100 }))
+    room.start() 
 
-    React.useEffect(() => {
-        console.log('hey')      
-        room.start() 
-    }, [])
-
-    useInterval(() => {
-        room.step() 
-    }, 100)
+    // useInterval(() => {
+    //     room.step() 
+    // }, 100)
 
     return (
         <div className="Room">
