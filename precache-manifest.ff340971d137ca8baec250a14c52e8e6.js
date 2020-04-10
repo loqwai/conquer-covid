cfc444fe828ad52398b99f802c1ea6393b81b446
redaphid <redaphid@users.noreply.github.com>
2020-04-10 03:51:34 +0000

    deploy: 36c31fd626398abc79a0701acb4e5a1548660e29

---
+++ precache-manifest.ff340971d137ca8baec250a14c52e8e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249025d2b0b25b5346cf680e0ec601ce",
+    "url": "/conquer-covid/index.html"
+  },
+  {
+    "revision": "c32ca6d53ab598f84fa1",
+    "url": "/conquer-covid/static/css/main.6ecf0d28.chunk.css"
+  },
+  {
+    "revision": "07e4daadbf17c9e049b5",
+    "url": "/conquer-covid/static/js/2.abaa4cc0.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/conquer-covid/static/js/2.abaa4cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c32ca6d53ab598f84fa1",
+    "url": "/conquer-covid/static/js/main.e78efe7e.chunk.js"
+  },
+  {
+    "revision": "22caa1c34900ca10117d",
+    "url": "/conquer-covid/static/js/runtime-main.0ca92d00.js"
+  }
+]);
