255c4ce2044ba1ed66a71678228824a23a2d9cd0
Roy van de Water <roy.vwater@gmail.com>
2020-04-14 16:57:18 -0700

    Hide the toolbar & minimap

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,26 @@ import Room from './models/room'
 import RoomComponent from './Room'
 import { Chance } from 'chance'
 import AutoSizer from 'react-virtualized-auto-sizer'
-import { ReactSVGPanZoom, TOOL_AUTO } from 'react-svg-pan-zoom' 
+import { UncontrolledReactSVGPanZoom } from 'react-svg-pan-zoom' 
 import './App.css'
 import {createPerson } from './models/room'
 import useInterval from './hooks/useInterval'
 import useAnimationFrame from './hooks/useAnimationFrame'
 const chance = new Chance()
 
-const popCount = 100
-const roomCount = 25
-const columnCount = 5
+const popCount = 90
+const roomCount = 9
+const columnCount = 3 
+const maxBigness = 400
+
+const rowCount = Math.floor(roomCount / columnCount)
 
 const createRooms = () => {
   const people = R.times(createPerson, popCount)
   chance.pickone(people).infected = true
   const roomPops = R.splitEvery(popCount / roomCount, people)
   return R.map(roomPop => {
-    const bigness = chance.integer({ min: 100, max: 400 })
+    const bigness = chance.integer({ min: 100, max: maxBigness })
     return new Room(roomPop, { height: bigness, width: bigness })
   }, roomPops)
 }
@@ -49,9 +52,6 @@ const App = () => {
 
   const [roomsData, setRoomsData] = React.useState(rooms.map(r => r.toData()))
 
-  const [tool, setTool] = React.useState(TOOL_AUTO)
-  const [value, setValue] = React.useState({})
-
   useAnimationFrame(() => {
     setRoomsData(rooms.map(r => r.toData()))
   })
@@ -61,22 +61,17 @@ const App = () => {
       <AutoSizer>
         {({width, height}) => {
           return width && height && (
-            <ReactSVGPanZoom 
-              tool={tool} 
-              value={value}
+            <UncontrolledReactSVGPanZoom 
               width={width}
               height={height}
-              onChangeTool={setTool}
-              onChangeValue={setValue}
+              tool="pan"
+              customToolbar={() => null}
+              customMiniature={() => null}
             >
-              <svg className="world" viewBox="0 0 1000 1000">
-                {roomsData.map((room, i) => {
-                  const column = i % columnCount
-                  const row = Math.floor(i / columnCount)
-                  return <RoomComponent key={i} row={row} column={column} {...room} />
-                })}
+              <svg className="world" viewBox={`0 0 ${columnCount * maxBigness} ${rowCount * maxBigness}`}>
+                {roomsData.map((room, i) => <RoomComponent key={i} {...room} row={Math.floor(i / columnCount)} column={i % columnCount} />)}
               </svg>
-            </ReactSVGPanZoom>
+            </UncontrolledReactSVGPanZoom>
           )
         }}
       </AutoSizer>
