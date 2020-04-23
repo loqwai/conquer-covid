967ebd7b657a3ba4c95f8be3c5e5cefcb9c7574a
redaphid <iam@aaronherres.com>
2020-04-22 23:23:22 -0700

    requestAnimationFrame()

--- src/App.tsx
+++ src/App.tsx
@@ -15,25 +15,26 @@ const moment = Moment as any
 const chance = new Chance()
 
 const App = () => {
-  const [count, setCount] = React.useState(0)  
+  const [time, setTime] = React.useState<number>(moment().unix())
+  const [frameDelta, setFrameDelta] = React.useState<number>(performance.now())  
   const requestRef = React.useRef(0)
   const previousTimeRef = React.useRef(0)
-  const animate = (time) => {  
-      const deltaTime = time - previousTimeRef.current
-      // Pass on a function to the setter of the state
-      // to make sure we always have the latest state
-    setCount(prevCount => (prevCount + deltaTime * 0.01) % 100)    
-    previousTimeRef.current = time
-    requestRef.current = requestAnimationFrame(animate)
-  }
+  
   React.useEffect(() => {
-    requestRef.current = requestAnimationFrame(animate);
-    return () => cancelAnimationFrame(requestRef.current);
-  }, []); // Make sure the effect runs only once
+    requestRef.current = requestAnimationFrame(animate)
+    return () => cancelAnimationFrame(requestRef.current)
+  }, [])
+
+  const animate = (t) => {  
+      console.log(frameDelta, t)
+      const deltaTime = t - previousTimeRef.current  
+      previousTimeRef.current = t
+      requestRef.current = requestAnimationFrame(animate)
+  }
   
   return (
     <div className="app">      
-      <h1 className="time">{count}</h1>
+      <h1 className="time">{time}</h1>
     </div>
   )
 }
